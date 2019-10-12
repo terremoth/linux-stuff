1bb80b3451726ee2fd19f0a8c4791e738ff275c4
Lucas M. Dutra <terremoth@users.noreply.github.com>
2019-10-12 19:05:27 -0300

    Add more software

--- install.sh
+++ install.sh
@@ -6,20 +6,68 @@ apt install sudo -y
 
 apt update && apt upgrade && apt dist-upgrade && apt autoremove
 
+apt install software-properties-common -y
+
+apt install apt-transport-https -y
+
+apt install gcc gdb vim nano bc sed gawk -y
+
 apt install build-essential -y
 
 apt install info -y
 
 apt install wget curl -y
 
-apt install dirmngr -y
+apt install gzip zip unzip wine -y
+
+apt install git gitflow -y
+
+apt install dirmngr dialog ccze -y
 
 apt install firmware-realtek -y
 
 apt install firmware-linux-nonfree -y
 
-apt install vlc
+apt install rhythmbox dosbox -y
 
-apt install xpdf
+apt install openjdk-* -y
+
+apt install gparted ncdu nmon slurm -y
+
+apt install qbittorrent pidgin -y
+
+apt install hardinfo -y
+
+apt install openssh-client htop -y
+
+apt install testdisk -y
+
+apt install libreoffice -y
+
+apt install ffmpeg youtube-dl audacity -y
+
+apt install vlc -y
+
+apt install xpdf -y
+
+apt install gimp inkscape -y
+
+add-apt-repository ppa:gezakovacs/ppa
+
+add-apt-repository ppa:ubuntuhandbook1/apps
+
+add-apt-repository ppa:fossfreedom/rhythmbox-plugins
+
+apt update -y
+
+apt install unetbootin -y
+
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ trusty main" | sudo tee /etc/apt/sources.list.d/brave-browser-release-trusty.list
+
+sudo apt update -y
+
+sudo apt install brave-browser -y
 
 # apt install google-chrome-stable
