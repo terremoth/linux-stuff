b8d6f19b23dc734657af2e3a4114a4d5d16e0474
Lucas M. Dutra <terremoth@users.noreply.github.com>
2020-06-07 02:41:23 -0300

    Update fun.sh

--- fun.sh
+++ fun.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-sudo apt install cmatrix 				        # creates matrix movie effect on screen
-sudo apt install screenfetch neofetch 	    # show ascii system information
+sudo apt install cmatrix					# creates matrix movie effect on screen
+sudo apt install screenfetch neofetch		# show ascii system information
 sudo apt install fortune 				    # random adage
 sudo apt install cowsay 				    # just let cow say something
 sudo apt install toilet 				    # draw nice ascii words with styles
