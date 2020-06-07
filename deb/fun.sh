5868ba34e0d79fc4e04694fa05d950deb847a64a
Lucas M. Dutra <terremoth@users.noreply.github.com>
2020-06-07 03:02:08 -0300

    Update and rename fun.sh to deb/fun.sh

---
+++ deb/fun.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+sudo apt install cmatrix -y                    # creates matrix movie effect on screen
+sudo apt install screenfetch neofetch -y       # show ascii system information
+sudo apt install fortune -y                    # random adage
+sudo apt install cowsay -y                     # just let cow say something
+sudo apt install toilet -y                     # draw nice ascii words with styles
+sudo apt install figlet -y                     # draw nice ascii words with styles
+sudo apt install espeak -y                     # speech synthesizer
+sudo apt install libaa-bin -y                  # ascii graphics
+sudo apt install sysvbanner -y                 # like toilet but limited to 10 chars
+sudo apt install aview -y                      # images to ascii art
+sudo apt install rig -y                        # generate fake data real-like
