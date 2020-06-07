184ad9a001e04fa1e61ba6548b8ebc6d575a7bec
Lucas M. Dutra <terremoth@users.noreply.github.com>
2020-06-07 14:27:50 -0300

    Update laravel.sh

--- deb/laravel.sh
+++ deb/laravel.sh
@@ -2,3 +2,5 @@
 sudo apt install php composer openssl php-bcmath php-json php-mbstring php-tokenizer php-zip php-common php-cli php-xml -y
 composer global require laravel/installer
 composer global require laravel/ui
+composer global require laravel/passport
+composer global require laravel/socialite
