d47f3a3b15f380eff979fb91e68e23c6349cecaa
Lucas M. Dutra <terremoth@users.noreply.github.com>
2020-06-07 02:32:59 -0300

    Update fun.sh

--- fun.sh
+++ fun.sh
@@ -1,15 +1,27 @@
 #!/bin/sh
 
-sudo apt install \      # beggin installation
-cmatrix                 # creates matrix movie effect on screen
-screenfetch neofetch \  # show ascii system information
-fortune                 # random adage
-cowsay \                # just let cow say something
-toilet \                # draw nice ascii words with styles 
-figlet \                # draw nice ascii words with styles 
-espeak \                # speech synthesizer
-libaa-bin \             # ascii graphics
-sysvbanner \            # like toilet but limited to 10 chars
-aview \                 # images to ascii art
-rig \                   # generate fake data real-like
--y
+# beggin installation
+sudo apt install \
+	# creates matrix movie effect on screen
+  	cmatrix \
+	# show ascii system information
+	screenfetch neofetch \
+	# random adage
+	fortune \
+	# just let cow say something
+	cowsay \
+	# draw nice ascii words with styles
+	toilet \
+	# draw nice ascii words with styles
+	figlet \
+	# speech synthesizer
+	espeak \
+	# ascii graphics
+	libaa-bin \
+	# like toilet but limited to 10 chars
+	sysvbanner \
+	# images to ascii art
+	aview \
+	# generate fake data real-like
+	rig \
+	-y
