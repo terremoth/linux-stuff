ee70335177a6141e7e87a36a99c0e407d4a56069
Lucas M. Dutra <terremoth@users.noreply.github.com>
2020-06-07 02:15:07 -0300

    Create fun.sh

---
+++ fun.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+sudo apt install \      # beggin installation
+cmatrix                 # creates matrix movie effect on screen
+screenfetch neofetch \  # show ascii system information
+fortune                 # random adage
+cowsay \                # just let cow say something
+toilet \                # draw nice ascii words with styles 
+figlet \                # draw nice ascii words with styles 
+espeak \                # speech synthesizer
+libaa-bin \             # ascii graphics
+sysvbanner \            # like toilet but limited to 10 chars
+aview \                 # images to ascii art
+rig \                   # generate fake data real-like
+-y
