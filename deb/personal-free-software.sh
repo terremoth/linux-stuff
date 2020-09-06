e94bab7b1ee14dd37e241b6d30df018acf33486a
Lucas M. Dutra <terremoth@users.noreply.github.com>
2020-09-06 03:46:47 -0300

    Create personal-free-software.sh

---
+++ deb/personal-free-software.sh
@@ -0,0 +1,11 @@
+!#/bin/sh
+
+sudo apt update -y
+sudo apt upgrade -y
+
+# office suite
+sudo apt install gnumeric -y
+sudo apt install abiword -y
+
+sudo apt install gnucash -y
+sudo apt install emacs-gtk -y
