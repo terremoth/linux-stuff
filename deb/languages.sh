3c1fbe3f5503d84e29d83c64c2ad55c043e44497
Lucas M. Dutra <terremoth@users.noreply.github.com>
2021-05-03 00:22:29 -0300

    Create languages.sh

---
+++ deb/languages.sh
@@ -0,0 +1,27 @@
+#!/bin/env sh
+
+sudo apt install lua5.4 luajit -y                   # Lua language
+sudo apt install yabasic -y                         # Yet another simple BASIC interpreter
+sudo apt install gnucobol -y                        # GNU Cobol
+sudo apt install pforth gforth -y                   # Portable and GNU Forth
+sudo apt install squirrel3 -y                       # Squirrel, a Smalltalk like language
+sudo apt install nim -y                             # Statically typed compiled programming language 
+sudo apt install algol68g -y                        # ALGOL 68 Implementation
+sudo apt install gcc gdb -y                         # GNU compilers collection + GNU Debugger (C, C++)
+sudo apt install default-jdk -y                     # Default Java Development Toolkit
+sudo apt install fasm nasm -y                       # most common assemblers
+sudo apt install python -y                          # Python language
+sudo apt install python-pip -y                      # Python repositories downloader/updater
+sudo apt install markdown -y                        # Markdown text to html
+sudo apt install php -y                             # PHP language
+sudo apt install composer -y                        # PHP repositories downloader/updater
+sudo apt install perl -y                            # PERL Language
+sudo apt install sqlite sqlite-doc -y               # Minimal SQL database
+sudo apt install hsbrainfuck -y                     # Brainfuck language interpreter
+sudo apt install icont icon-ipl iiconx -y           # icon programming language similar to algol and snobol
+sudo apt install aplus-fsf aplus-fsf-dev aplus-doc  # A+ language, derived from APL
+sudo apt install afnix afnix-doc -y                 # Multi-threaded functional that support the object oriented paradigm
+sudo apt install chuck -y                           # audio programming language
+
+# Scheme implementations
+sudo apt install ikarus tinyscheme chibi-scheme chezscheme newlisp -y 
