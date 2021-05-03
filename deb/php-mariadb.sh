582aade31155b53cd4d966048121534be1a04bd7
Lucas M. Dutra <terremoth@users.noreply.github.com>
2021-05-03 00:54:10 -0300

    Update php-mariadb.sh

--- deb/php-mariadb.sh
+++ deb/php-mariadb.sh
@@ -1,7 +1,7 @@
 #!/bin/env sh
 
 sudo apt install php php-bz2 php-bcmath php-cgi php-cli php-common php-curl php-date php-db php-enchant \
-php-embed php-fpm php-gd php-gmp php-imap php-intl php-json php-json-schema php-mail php-mail-mime php-php-mbstring php-memcache \
+php-embed php-fpm php-gd php-gmp php-imap php-intl php-json php-json-schema php-mail php-mail-mime php-mbstring php-memcache \
 php-memcached php-mime-type php-mongodb php-mysql php-pear php-pgsql php-readline php-snmp php-soap \ 
 php-sqlite3 php-ssh2 php-php-xdebug php-xml php-xmlrpc php-zip \
 mariadb-server mariadb-client -y
