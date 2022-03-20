e578bbb5cdce0459c32c1974a3776e1d7b0fbec8
Lucas M. Dutra <terremoth@users.noreply.github.com>
2022-03-20 00:08:07 -0300

    Create laravel-project.sh

---
+++ deb/laravel-project.sh
@@ -0,0 +1,8 @@
+#!/bin/env sh
+composer require laravelcollective/html
+composer require laravel/ui
+composer require laravel/helpers
+composer require guzzlehttp/guzzle
+composer require --dev barryvdh/laravel-ide-helper
+composer require --dev fakerphp/faker
+composer require --dev phpunit/phpunit
