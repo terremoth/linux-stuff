2ed8be80f8be97baa7cc982a0090da608dfe0f81
Lucas M. Dutra <terremoth@users.noreply.github.com>
2022-03-20 00:13:02 -0300

    Laravel only install with its minimal php dependencies

--- deb/laravel.sh
+++ deb/laravel.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
-sudo apt install php composer openssl php-bcmath php-json php-mbstring php-tokenizer php-zip php-common php-cli php-xml -y
+sudo apt install php composer openssl php-bcmath php-curl php-json php-mbstring php-tokenizer php-zip php-common php-cli php-xml -y
 composer global require laravel/installer
-composer global require laravel/ui
-composer global require laravel/passport
-composer global require laravel/socialite
