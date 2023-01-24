edcebb02f2377c37c54150168e585da435223e5b
Lucas M. Dutra <terremoth@users.noreply.github.com>
2023-01-24 13:07:10 -0300

    Update languages.sh

--- deb/languages.sh
+++ deb/languages.sh
@@ -10,16 +10,16 @@ sudo apt install algol68g -y                        # ALGOL 68 Implementation
 sudo apt install gcc gdb -y                         # GNU compilers collection + GNU Debugger (C, C++)
 sudo apt install default-jdk -y                     # Default Java Development Toolkit
 sudo apt install fasm nasm -y                       # most common assemblers
-sudo apt install python -y                          # Python language
-sudo apt install python-pip -y                      # Python repositories downloader/updater (package manager)
+sudo apt install python3 -y                         # Python language
+sudo apt install python3-pip -y                     # Python repositories downloader/updater (package manager)
 sudo apt install markdown -y                        # Markdown text to html
 sudo apt install php -y                             # PHP language
 sudo apt install composer -y                        # PHP repositories downloader/updater (package manager)
 sudo apt install perl -y                            # PERL Language
 sudo apt install sqlite sqlite-doc -y               # Minimal SQL database
 sudo apt install hsbrainfuck -y                     # Brainfuck language interpreter
-sudo apt install icont icon-ipl iiconx -y           # icon programming language similar to algol and snobol
-sudo apt install aplus-fsf aplus-fsf-dev aplus-doc  # A+ language, derived from APL
+sudo apt install icont icon-ipl iconx -y            # icon programming language similar to algol and snobol
+sudo apt install aplus-fsf aplus-fsf-dev aplus-fsf-doc  # A+ language, derived from APL
 sudo apt install afnix afnix-doc -y                 # Multi-threaded functional that support the object oriented paradigm
 sudo apt install chuck -y                           # audio programming language
 
