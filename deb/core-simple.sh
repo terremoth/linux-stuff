eb0225dd3d7d27475f37807a4c6b04970ab857d8
Lucas M. Dutra <terremoth@users.noreply.github.com>
2023-09-27 15:02:35 -0300

    Create core-simple.sh

---
+++ deb/core-simple.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# I am sorry, this is for good (for me - you can remove these 5 lines below):
+sudo apt purge libreoffice -y
+sudo apt purge libreoffice-* -y
+sudo systemctl stop snapd
+sudo apt purge snap -y
+sudo apt purge snapd -y
+sudo apt remove bluedevil quassel quassel-data qtpass qlipper noblenote -y
+
+
+# Update and Upgrade system packages before installation
+sudo apt update -y
+sudo apt upgrade -y
+
+sudo apt install $(cat core.pkgs) -y
