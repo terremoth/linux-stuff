a5b55518b96c7d9487c18671ddd8e2842eadaad0
Lucas M. Dutra <terremoth@users.noreply.github.com>
2024-02-05 13:25:08 -0300

    Update core.sh

--- deb/core.sh
+++ deb/core.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
-# I am sorry, this is for good (for me - you can remove these lines below):
+# I am sorry, this is for good (for me - you can remove these lines below) - kill and purge snap forever:
 sudo apt purge libreoffice -y
 sudo apt purge libreoffice-* -y
 sudo snap disable firefox
 sudo snap remove --purge firefox
+sudo snap remove snap-store
+sudo apt purge gnome-software-plugin-snap
 sudo systemctl stop snapd
+
 sudo apt purge snap -y
 sudo apt purge snapd -y
+
+# Remove some specific Qt apps I don't use
 sudo apt remove bluedevil quassel quassel-data qtpass qlipper noblenote -y
 
 # Update and Upgrade system packages before installation
