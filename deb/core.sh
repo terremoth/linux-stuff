54087a035c7d9df5e2f99b865ca2ebc2f754c42d
Lucas M. Dutra <terremoth@users.noreply.github.com>
2024-02-05 14:37:43 -0300

    Remove telemetry from ubuntu

--- deb/core.sh
+++ deb/core.sh
@@ -15,6 +15,21 @@ sudo apt purge snapd -y
 # Remove some specific Qt apps I don't use
 sudo apt remove bluedevil quassel quassel-data qtpass qlipper noblenote -y
 
+# Let's remove/purge any telemetry enabled:
+
+echo "[*] Resolving \"metrics.ubuntu.com\" to localhost"
+echo 127.0.0.1 www.metrics.ubuntu.com >>/etc/hosts
+echo 127.0.0.1 metrics.ubuntu.com >>/etc/hosts
+
+echo "[*] Resolving \"popcon.ubuntu.com\" to localhost"
+echo 127.0.0.1 www.popcon.ubuntu.com >>/etc/hosts
+echo 127.0.0.1 popcon.ubuntu.com >>/etc/hosts
+
+# == Remove and blacklist telemetry services:
+echo "[*] Removing telemetry services"
+apt purge -y ubuntu-report popularity-contest apport whoopsie apport-symptoms >/dev/null 2>&1 && apt-mark hold ubuntu-report popularity-contest apport whoopsie apport-symptoms
+ubuntu-report -f send no
+
 # Update and Upgrade system packages before installation
 sudo apt update -y
 sudo apt upgrade -y
