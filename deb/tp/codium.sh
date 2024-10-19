25e75658346d6057212d2784bc04f2f94373742f
Lucas M. Dutra <terremoth@users.noreply.github.com>
2024-10-19 05:03:19 -0300

    Create codium.sh

---
+++ deb/tp/codium.sh
@@ -0,0 +1,10 @@
+#!/bin/env sh
+
+wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
+    | gpg --dearmor \
+    | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
+
+echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://download.vscodium.com/debs vscodium main' \
+    | sudo tee /etc/apt/sources.list.d/vscodium.list
+
+sudo apt update && sudo apt install codium
