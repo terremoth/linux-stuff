2a8f3588caa8b1ebe82b5a7a9768272efd36eae4
Lucas M. Dutra <terremoth@users.noreply.github.com>
2024-10-22 12:15:38 -0300

    Create simple-games.sh

---
+++ deb/simple-games.sh
@@ -0,0 +1,3 @@
+#!/bin/env sh
+
+sudo apt install -y supertux supertuxkart tuxfootball frogatto crispy-doom acm bzflag  extremetuxracer  torcs trigger-rally redeclipse tuxpuck neverball pinball trackballs foobillardplus pong2 petris ardentryst freedroidrpg 2048-qt aisleriot kpat  frozen-bubble pacman snake4 ksudoku astromenace chromium-bsu endless-sky open-invaders kobodeluxe 
